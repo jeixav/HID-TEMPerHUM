35a531263b2a0761f0b2fb3eaae389bb85198017
Valarie Moore <jeixav@gmail.com>
2009-12-28 15:53:38 -0600

    Cosmetic changes.

--- temper.c
+++ temper.c
@@ -7,25 +7,29 @@
  * Temper.c by Robert Kavaler (c) 2009 (relavak.com)
  * All rights reserved.
  *
- * Temper driver for linux. This program can be compiled either as a library
- * or as a standalone program (-DUNIT_TEST). The driver will work with some
- * TEMPer usb devices from RDing (www.PCsensor.com).
+ * Temper driver for linux. This program can be compiled either as a
+ * library or as a standalone program (-DUNIT_TEST). The driver will
+ * work with some TEMPer usb devices from RDing (www.PCsensor.com).
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer.
  * 
  * THIS SOFTWARE IS PROVIDED BY Robert Kavaler ''AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL Robert kavaler BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Robert kavaler BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
  * 
  */
 
@@ -199,12 +203,13 @@ TemperGetTempAndRelHum(Temper *t, float *tempC, float *relhum)
 		TemperSendCommand(t, 0, 0, 0, 0, 0, 0, 0, 0);
 	}
 	TemperSendCommand(t, 10, 11, 12, 13, 0, 0, 1, 0);
-	/* According to Sensirion datasheet for SHT1x the time for 8/12/14 bit measurements is 20/80/320 ms. Trial and error suggests that 400ms is a safe least upper bound */
-	usleep(400000);
-	ret = TemperGetData(t, buf, 256);
-	if(ret < 2) {
-		return -1;
-	}
+
+	/* According to Sensirion datasheet for SHT1x the time for
+	8/12/14 bit measurements is 20/80/320 ms. Trial and error
+	suggests that sleeping less that 400ms can produce spurious
+	measurements */
+	usleep(400000); ret = TemperGetData(t, buf, 256); if(ret < 2)
+	{ return -1; }
 
 	if(t->debug) {
 	  printf("TempAndRelHum (%d bytes):\n", ret);
